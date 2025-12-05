8a6b9b01d2ff2e6ebec752e228ec1c013f5ac5a1
Akhil Naidu <akhilnaid1997@gmail.com>
2025-12-05 20:13:14 +0530

    k8s

--- workstattion.sh
+++ workstattion.sh
@@ -19,7 +19,3 @@ PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
-
-git clone https://github.com/akhilnaidu1997/eksctl.git
-cd eksctl
-eksctl create cluster --config-file=eks.yaml
